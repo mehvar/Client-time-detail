8027c1a3fb8202e30d139a8d0cd718783f4d42b3
mehvar <justmehvar@gmail.com>
2017-10-25 06:40:35 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<body onload="setTimeout( 0);" oncontextmenu="return false" onkeydown="return false" bgcolor="grey">
+
+Your Current Day/time detail is '<script type="text/javascript">
+var x = new Date()
+document.write(x)
+</script>';
